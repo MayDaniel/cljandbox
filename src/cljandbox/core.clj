47843e79882703d1c0c45940ee45934edb9d3801
MayDaniel <yamnad1@gmail.com>
2010-09-21 22:37:06 +0100

    Added a couple of docstrings in the form of examples; because I am lazy.

--- src/cljandbox/core.clj
+++ src/cljandbox/core.clj
@@ -10,10 +10,13 @@
 (defalias fn-or fn/or)
 (defalias fn-not fn/not)
 
-(defn <- [coll]
+(defn <- "(filter identity coll)" [coll]
   (filter identity coll))
 
 (defmacro ->_
+  "(macroexpand-all '(->_ 5 (+ 1 _) (- 5 _ 5) (/ _ 5)))
+
+  ;; (/ (- 5 (+ 1 5) 5) 5)"
   ([x] x)
   ([x form] (if (seq? form)
               (with-meta `(~@(postwalk-replace {'_ x} form)) (meta form))
