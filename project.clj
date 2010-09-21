8c1272c376858efd695cfa71afeb64de8cde75c0
MayDaniel <yamnad1@gmail.com>
2010-09-21 22:56:45 +0100

    Added :url to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject cljandbox "0.1.0-SNAPSHOT"
+  :url "http://maydaniel.github.com/cljandbox/"
   :dependencies [[clojure "1.2.0"]
                  [fn "1.0.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
