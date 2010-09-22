da3df01b77b477b839f0a93dad4fb9bf8a0805a6
MayDaniel <yamnad1@gmail.com>
2010-09-22 16:12:18 +0100

    Added somnium.congomongo.util/defunk

---
+++ src/cljandbox.clj
@@ -0,0 +1,28 @@
+(ns cljandbox
+  (:require fn
+            [somnium.congomongo.util :as congo])
+  (:use [clojure.walk :only [postwalk-replace]]
+        [clojure.contrib.def :only [defalias]]))
+
+;; http://github.com/somnium/congomongo
+
+(defalias defunk congo/defunk)
+
+;; http://github.com/zahardzhan/fn
+
+(defalias fn-and fn/and)
+(defalias fn-or fn/or)
+(defalias fn-not fn/not)
+
+(defn <-
+  "Returns a lazy sequence of the items in coll that are boolean true."
+  [coll]
+  (filter identity coll))
+
+(defmacro ->_
+  "Threads the forms, replacing underscores with the result of the last expression."
+  ([x] x)
+  ([x form] (if (seq? form)
+              (with-meta (postwalk-replace {'_ x} form) (meta form))
+              (list form x)))
+  ([x form & more] `(->_ (->_ ~x ~form) ~@more)))
