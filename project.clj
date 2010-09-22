26c26125fb2dd067e3068041e2f8097b0e6d7902
MayDaniel <yamnad1@gmail.com>
2010-09-22 16:12:07 +0100

    Added somnium.congomongo.util/defunk

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject cljandbox "0.1.0-SNAPSHOT"
   :url "http://maydaniel.github.com/cljandbox/"
   :dependencies [[clojure "1.2.0"]
+                 [congomongo "0.1.3-SNAPSHOT"]
                  [fn "1.0.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]
                      [autodoc "0.7.1-SNAPSHOT"]])
