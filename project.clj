39104e6b733d4df26c39e285005cb62804ded32f
MayDaniel <yamnad1@gmail.com>
2010-09-23 22:00:42 +0100

    clojure-contrib.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljandbox "0.1.1"
   :description "A collection of Clojure utilities."
   :url "http://maydaniel.github.com/cljandbox/"
-  :dependencies [[clojure "1.2.0"]]
+  :dependencies [[clojure "1.2.0"] [clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"] [autodoc "0.7.1-SNAPSHOT"]])
