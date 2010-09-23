296927d7e5d824db351e018f315d0da73c334f3f
MayDaniel <yamnad1@gmail.com>
2010-09-23 19:01:16 +0100

    Updated version, cleaned project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject cljandbox "0.1.0-SNAPSHOT"
+(defproject cljandbox "0.1.0"
+  :description "A collection of Clojure utilities."
   :url "http://maydaniel.github.com/cljandbox/"
-  :dependencies [[clojure "1.2.0"]
-                 [fn "1.0.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]
-                     [autodoc "0.7.1-SNAPSHOT"]])
+  :dependencies [[clojure "1.2.0"] [fn "1.0.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"] [autodoc "0.7.1-SNAPSHOT"]])
