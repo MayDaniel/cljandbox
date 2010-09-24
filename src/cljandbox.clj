4f959b476a0732322fc062124aa0e714c00e214a
MayDaniel <yamnad1@gmail.com>
2010-09-24 17:49:22 +0100

    Added defsource.

--- src/cljandbox.clj
+++ src/cljandbox.clj
@@ -3,6 +3,36 @@
   (:use [clojure.walk :only [postwalk-replace]]
         [clojure.contrib.def :only [defnk]]))
 
+(defmacro ->_
+  "Threads the forms, replacing underscores with the result of the last expression."
+  ([x] x)
+  ([x form] (if (seq? form)
+              (with-meta (postwalk-replace {'_ x} form) (meta form))
+              (list form x)))
+  ([x form & more] `(->_ (->_ ~x ~form) ~@more)))
+
+(defmacro defsource
+  "Similar to clojure.core/defn, but saves the function's definition in the var's
+   :source metadata."
+  {:arglists (:arglists (meta (var defn)))}
+  [fn-name & defn-stuff]
+  `(do (defn ~fn-name ~@defn-stuff)
+       (alter-meta! (var ~fn-name) assoc :source (quote ~&form))
+       (var ~fn-name)))
+
+(defmacro defunk
+  "Similar to clojure.contrib.def/defnk, but accepts :arglists metadata."
+  [fn-name & fn-tail]
+  (let [arglists (some :arglists fn-tail)]
+    `(do (defnk ~fn-name ~@fn-tail)
+         (when ~arglists (alter-meta! (var ~fn-name) assoc :arglists ~arglists))
+         (var ~fn-name))))
+
+(defn <-
+  "Returns a lazy sequence of the items in coll that are boolean true."
+  [coll]
+  (filter identity coll))
+
 (defn of-all?
   "(of-all? f g ...)
    => (fn [& xs] (and (apply f xs) (apply g xs) ... true))
@@ -26,24 +56,3 @@
   ((of-none? integer? string?) {}) will yield true."
   [& predicates]
   (fn [& xs] (not-any? #(apply % xs) predicates)))
-
-(defn <-
-  "Returns a lazy sequence of the items in coll that are boolean true."
-  [coll]
-  (filter identity coll))
-
-(defmacro defunk
-  "Similar to clojure.contrib.def/defnk, but accepts :arglists meta-data."
-  [fn-name & fn-tail]
-  (let [arglists (some :arglists fn-tail)]
-    `(do (defnk ~fn-name ~@fn-tail)
-         (when ~arglists (alter-meta! (var ~fn-name) assoc :arglists ~arglists))
-         (var ~fn-name))))
-
-(defmacro ->_
-  "Threads the forms, replacing underscores with the result of the last expression."
-  ([x] x)
-  ([x form] (if (seq? form)
-              (with-meta (postwalk-replace {'_ x} form) (meta form))
-              (list form x)))
-  ([x form & more] `(->_ (->_ ~x ~form) ~@more)))
