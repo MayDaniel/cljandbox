4f7cc9fffccb206eac174765cdfe1fc789e7d461
MayDaniel <yamnad1@gmail.com>
2010-10-17 17:50:08 +0100

    partialr

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljandbox "0.1.1"
+(defproject cljandbox "0.1.2"
   :description "A collection of Clojure utilities."
   :url "http://maydaniel.github.com/cljandbox/"
   :dependencies [[clojure "1.2.0"] [clojure-contrib "1.2.0"]]
