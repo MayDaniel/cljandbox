467d0111a23547ae85420c20b5d2473921e5401c
unknown <Dan@.(none)>
2010-12-11 21:25:13 +0000

    '0.2.0'

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "A collection of Clojure utilities."
   :url "http://maydaniel.github.com/cljandbox/"
   :dependencies [[clojure "1.2.0"] [clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"] [lein-clojars "0.6.0"]]
-  :tasks [cake-autodoc.tasks])
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"] [lein-clojars "0.6.0"]])
