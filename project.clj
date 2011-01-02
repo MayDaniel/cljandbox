e767ebbf653dff20597a216e4db53a470659f786
unknown <Dan@.(none)>
2011-01-02 19:40:21 +0000

    cljandbox

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject cljandbox "0.2.1"
+(defproject cljandbox "0.2.2"
   :description "A collection of Clojure utilities."
   :url "http://maydaniel.github.com/cljandbox/"
-  :dependencies [[clojure "1.2.0"] [clojure-contrib "1.2.0"]
-                 [cljandbox "0.2.1"]])
+  :dependencies [[clojure "1.2.0"] [clojure-contrib "1.2.0"]])
